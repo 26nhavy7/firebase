aab09edb3ccb94f044d5ab09782d85a684b4a1d2
26nhavy7 <26nhavy7@gmail.com>
2026-08-05 16:53:34 -0400

    first commit

--- init-firebase.js
+++ init-firebase.js
@@ -2,7 +2,15 @@
  * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
  */
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyDKzxpUohvuMp30ti6KeSh92_NTytLJPeo",
+  authDomain: "fire-chat-1ff08.firebaseapp.com",
+  databaseURL: "https://fire-chat-1ff08-default-rtdb.firebaseio.com",
+  projectId: "fire-chat-1ff08",
+  storageBucket: "fire-chat-1ff08.firebasestorage.app",
+  messagingSenderId: "995120791614",
+  appId: "1:995120791614:web:634c1150925d61d280916f"
+};
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,92 +1,46 @@
-/**
- * @TODO get a reference to the Firebase Database object
- */
+function makeSingleMessageHTML(usernameTxt, emailTxt, messageTxt) {
 
-/**
- * @TODO get const references to the following elements:
- *      - div with id #all-messages
- *      - input with id #username
- *      - input with id #message
- *      - button with id #send-btn and the updateDB
- *        function as an onclick event handler
- */
+    // Create parent div
+    let parentDiv = document.createElement("div");
+    parentDiv.classList.add("single-message");
 
-/**
- * @TODO create a function called updateDB which takes
- * one parameter, the event, that:
- *      - gets the values of the input elements and stores
- *        the data in a temporary object with the keys USERNAME
- *        and MESSAGE
- *      - console.logs the object above
- *      - writes this object to the database
- *      - resets the value of #message input element
- */
+    // Username
+    let userPTag = document.createElement("p");
+    userPTag.innerHTML = "<strong>Username:</strong> " + usernameTxt;
 
-function updateDB(event) {
-  // Prevent default refresh
-  // Create data object
-  // console.log the object
-  // GET *PUSH* PUT DELETE
-  // Write to our database
-  // Reset message
+    // Email
+    let emailPTag = document.createElement("p");
+    emailPTag.innerHTML = "<strong>Email:</strong> " + emailTxt;
+
+    // Message
+    let messagePTag = document.createElement("p");
+    messagePTag.innerHTML = "<strong>Message:</strong> " + messageTxt;
+
+    // Add everything to the parent div
+    parentDiv.appendChild(userPTag);
+    parentDiv.appendChild(emailPTag);
+    parentDiv.appendChild(messagePTag);
+
+    // Return the completed message
+    return parentDiv;
 }
+const sendBtn = document.getElementById("send-btn");
+const allMessagesContainer = document.getElementById("all-messages");
 
-/**
- * @TODO add the addMessageToBoard function as an event
- * handler for the "child_added" event on the database
- * object
- */
 
-/**
- * @TODO create a function called addMessageToBoard that
- * takes one parameter rowData which:
- *      - console.logs the data within rowData
- *      - creates a new HTML element for a single message
- *        containing the appropriate data
- *      - appends this HTML to the div with id
- *        #all-messages (we should have a reference already!)
- *
- */
 
-function addMessageToBoard(rowData) {
-  // Store the value of rowData inside object named 'data'
-  // console.log data
-  // Create a variable named singleMessage
-  // that stores function call for makeSingleMessageHTML()
-  // Append the new message HTML element to allMessages
-}
 
-/**
- * @TODO create a function called makeSingleMessageHTML which takes
- * two parameters, usernameTxt and messageTxt, that:
- *      - creates a parent div with the class .single-message
- *
- *      - creates a p tag with the class .single-message-username
- *      - update the innerHTML of this p to be the username
- *        provided in the parameter object
- *      - appends this p tag to the parent div
- *
- *      - creates a p tag
- *      - updates the innerHTML of this p to be the message
- *        text provided in the parameter object
- *      - appends this p tag to the parent div
- *
- *      - returns the parent div
- */
 
-function makeSingleMessageHTML(usernameTxt, messageTxt) {
-  // Create Parent Div
-  // Add Class name .single-message
-  // Create Username P Tag
-  // Append username
-  // Create message P Tag
-  // Return Parent Div
-}
+sendBtn.onclick = function () {
+    let usernameVal = document.getElementById("username").value;
+    let emailVal = document.getElementById("email").value;
+    let messageVal = document.getElementById("message").value;
 
-/**
- * @BONUS add an onkeyup event handler to the form HTML
- * element so the user can also submit the form with the
- * Enter key
- *
- * @BONUS use an arrow function
- */
+    if (usernameVal !== "" && messageVal !== "") {
+        let messageElement = makeSingleMessageHTML(usernameVal, emailVal, messageVal);
+        allMessagesContainer.appendChild(messageElement);
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+    }
+};
